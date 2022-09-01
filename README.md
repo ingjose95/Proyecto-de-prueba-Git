4f3569a9c19b3d0e28af36ca4b6c09d16e42061b
Jose Melendez <jomelendezgm95@gmail.com>
2022-08-31 20:33:44 -0400

    Incorporacion de Modulos

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 Esta es una muestra de cómo usar Git.
+Estoy aprendiendo a crear commits y nuevas ramas. Ejemplo, esta es una rama.
--- script.py
+++ script.py
@@ -1,3 +1,7 @@
+from funtions import sumar, mensaje
+
 msg = 'mi name is Jose Melendez'
 
 print(f'Hello world, {msg}')
+
+print(f'Esta es una seuma: {sumar(100,50)}')
